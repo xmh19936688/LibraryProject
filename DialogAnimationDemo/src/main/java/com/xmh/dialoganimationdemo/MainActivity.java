e7590e41ea64a5d978aecec3e058fe230faee52b
xmh19936688 <xmh19936688@163.com>
2015-10-19 18:07:05 +0800

    Update DialogAnimationDemo

--- DialogAnimationDemo/src/main/java/com/xmh/dialoganimationdemo/MainActivity.java
+++ DialogAnimationDemo/src/main/java/com/xmh/dialoganimationdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xmh.dialoganimationdemo;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Dialog dialog = new Dialog(this,R.style.dialog_style);
+        dialog.setContentView(R.layout.dialog_loading);
+        dialog.setCanceledOnTouchOutside(true);//点击空白处消失
+        dialog.getWindow().setWindowAnimations(R.style.dialog_style);//dialog的动画要设置在window上
+        dialog.show();
+
     }
+
 }
