7cea632f88417c11849d7e92e3104a49776f3735
xumenghao <xumenghao@evolvingera.cn>
2015-11-10 00:59:21 +0800

    update little

--- DoubleBackExitDemo/src/main/java/com/xmh/doublebackexitdemo/BaseActivity.java
+++ DoubleBackExitDemo/src/main/java/com/xmh/doublebackexitdemo/BaseActivity.java
@@ -1,40 +1,12 @@
 package com.xmh.doublebackexitdemo;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
 import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
 
-    public static final int SHOW_TOAST_MSG = 100;
     private long clickTime = 0;              //记录第一次点击的时间
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    /*Toast提示处理*/
-    @SuppressLint("HandlerLeak")
-    public Handler mToastHandler = new Handler(){
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SHOW_TOAST_MSG:
-                    String str = (String)msg.obj;
-                    if(!TextUtils.isEmpty(str)){
-                        Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-    };
 
     /**
      * 退出应用
