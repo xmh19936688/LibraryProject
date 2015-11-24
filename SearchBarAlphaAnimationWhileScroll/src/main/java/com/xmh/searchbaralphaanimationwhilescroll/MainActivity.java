689b26c9212e86cedd6f9e763125c53cf543fcd3
xmh19936688 <xmh19936688@163.com>
2015-11-24 15:48:48 +0800

    update finish(use touch)

--- SearchBarAlphaAnimationWhileScroll/src/main/java/com/xmh/searchbaralphaanimationwhilescroll/MainActivity.java
+++ SearchBarAlphaAnimationWhileScroll/src/main/java/com/xmh/searchbaralphaanimationwhilescroll/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 
 import butterknife.Bind;
@@ -31,18 +32,62 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         ptrContent.getId();
 
+        tvSearch.getBackground().setAlpha(0);
 
         ScrollView scrollView = ptrContent.getRefreshableView();
 
-//        scrollView.setOnTouchListener(new OnContentTouchListener());
+        scrollView.setOnTouchListener(new OnContentTouchListener());
 
 
-        scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+//        scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+//            @Override
+//            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//
+//            }
+//        });
+
+        ptrContent.setOnPullEventListener(new PullToRefreshBase.OnPullEventListener<ScrollView>() {
             @Override
-            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-
+            public void onPullEvent(PullToRefreshBase<ScrollView> refreshView, PullToRefreshBase.State state, PullToRefreshBase.Mode direction) {
+                switch (state){
+                    case PULL_TO_REFRESH:
+                        tvSearch.setVisibility(View.GONE);
+                        Log.e("state_xmh","PULL_TO_REFRESH");
+                        break;
+                    case RESET:
+                        tvSearch.setVisibility(View.VISIBLE);
+                        Log.e("state_xmh","RESET");
+                        break;
+                    case RELEASE_TO_REFRESH:
+                        Log.e("state_xmh","RELEASE_TO_REFRESH");
+                        break;
+                    case REFRESHING:
+                        Log.e("state_xmh","REFRESHING");
+                        break;
+                    case MANUAL_REFRESHING:
+                        Log.e("state_xmh","MANUAL_REFRESHING");
+                        break;
+                    case OVERSCROLLING:
+                        Log.e("state_xmh","OVERSCROLLING");
+                        tvSearch.setVisibility(View.VISIBLE);
+                        tvSearch.getBackground().setAlpha(0);
+                        break;
+                }
             }
         });
+
+        ptrContent.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ScrollView>() {
+            @Override
+            public void onRefresh(final PullToRefreshBase<ScrollView> refreshView) {
+                refreshView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshView.onRefreshComplete();
+                    }
+                },1000);
+            }
+        });
+
     }
 
     private class  OnContentTouchListener implements View.OnTouchListener{
